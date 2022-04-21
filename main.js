b1f06c359fc9f7c3d3d5922741b17f3c0d4da2ac
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-04-22 01:09:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let simpleMultiplication =number => number%2==0 ? number*8 : number*9
+
+
+
+/* 8kyu simple multiplication
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+
+
